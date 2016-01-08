a0910fdb15d0d540e9907c5c0eb164d0fe176b90
Jay <jay@jaydemac-mini.local>
2016-01-08 10:08:37 +0800

    Initial Commit

---
+++ TQSomething/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TQSomething
+//
+//  Created by Jay on 16/1/8.
+//  Copyright © 2016年 TQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
